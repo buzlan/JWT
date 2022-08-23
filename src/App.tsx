edd1f29cee61bf8fc1232e5c6dc95d91e80d40d5
Ihar Buzlanau <Ihar.buzlanau@crealogix.com>
2022-08-23 16:47:09 +0300

    added FE part JWT tokens

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Dashboard } from "./components/dashboard/dashboard";
+import { Header } from "./components/header/header";
+import { Main } from "./components/main/main";
+
+import { IRootState, useAppDispatch } from "./store";
+import { getProfile } from "./store/auth/actionCreators";
 
 function App() {
+  const isLoggedIn = useSelector(
+    (state: IRootState) => !!state.auth.authData.accessToken
+  );
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getProfile());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route
+          path="/dashboard"
+          element={isLoggedIn ? <Dashboard /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/main/main.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { IRootState, useAppDispatch } from "../../store";
+import { getProfile, logoutUser } from "../../store/auth/actionCreators";
+import { Login } from "../login/login";
+
+export const Main = () => {
+  const dispatch = useAppDispatch();
+
+  const profile = useSelector(
+    (state: IRootState) => state.auth.profileData.profile
+  );
+  const isLoggedIn = useSelector(
+    (state: IRootState) => !!state.auth.authData.accessToken
+  );
+
+  const renderProfile = () => (
+    <div>
+      <div>Вы успeшно авторизовались, {profile}</div>
+      <button onClick={() => dispatch(logoutUser())}>Logout</button>
+      <button onClick={() => dispatch(getProfile())}>update profile</button>
+    </div>
+  );
+
+  return (
+    <div>
+      <h1>Main</h1>
+      {isLoggedIn ? renderProfile() : <Login />}
+    </div>
+  );
+};
